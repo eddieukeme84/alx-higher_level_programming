ae2c78d1ea6071b146ba3c52a42162f888715ba3
eddieukeme84 <eddieukeme84@gmail.comgit config --global user.email eddieukeme84@gmail.com>
2022-12-01 22:15:41 +0000

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Print the sum of 1 and 2."""
-    from add_0 import add
+    """Print the value of variable a from variable_load_5."""
+    from variable_load_5 import a
 
-    a = 1
-    b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    print(a)
